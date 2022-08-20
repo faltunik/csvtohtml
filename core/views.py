a5bf3185cd0ef344e6391c3d6f8510d8de316c33
faltunik <niksanat8@gmail.com>
2022-08-20 10:58:52 +0530

    done search and sorting feature

--- core/views.py
+++
@@ -1,5 +0,0 @@
-
-
-# we need a function that reads csv file and convert it in json
-
-# we need a file that takes json and 
--- mywork/views.py
+++ mywork/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import render
+import csv
 
-# Create your views here.
+
+def index(request):
+    # opening the csv file
+    main = []
+    word = request.GET.get("query", "")
+    ky = int(request.GET.get("key", 0))
+
+    print(word)
+    print(type(word))
+
+    with open('mywork\example.csv', mode ='r')as csv_file:
+        
+        csvFile = csv.reader(csv_file)
+        lst = list(csvFile)
+        for i in range(len(lst)):
+            if i ==0: continue
+            for wrd in lst[i]:
+                if word in wrd.lower():
+                    main.append(lst[i])
+                    break
+        
+    main = sorted(main, key = lambda x: x[ky])
+    print("main list is: ", main)
+    return render(request, "mywork/index.html", {
+        "details": main,
+        "query": word,
+        "key": ky
+    })
+
+
